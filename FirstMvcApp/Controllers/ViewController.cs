8af11cc124331ec4f96fb7a2176338e145f923b8
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-03 23:21:54 +0300

    Action , view ve partial view test edildi.

---
+++ FirstMvcApp/Controllers/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class ViewController : Controller
+    {
+        // GET: View
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ViewResult  Index3()
+        {
+            return View("Index");
+        }
+        public PartialViewResult Partial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Partial2()
+        {
+            return PartialView("Partial");
+        }
+    }
+}
