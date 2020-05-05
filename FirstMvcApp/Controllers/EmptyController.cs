dd67a39c11cffdf8931afaa466c6a40310074915
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-05 20:10:38 +0300

    empty result tamamlandı.

---
+++ FirstMvcApp/Controllers/EmptyController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class EmptyController : Controller
+    {
+       public void Index()
+        {
+
+        }
+
+        public ActionResult Index2()
+        {
+            return null;
+        }
+
+        public ActionResult Index3()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return new EmptyResult();
+            }
+            return Content("Welcome, kubi");
+        }
+    }
+}
