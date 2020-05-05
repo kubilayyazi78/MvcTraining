ae013ce85285971f36dff95d0ab89dc0a76a829a
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-05 19:56:05 +0300

    File result tamamlandı.

---
+++ FirstMvcApp/Controllers/FileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class FileController : Controller
+    {
+        public FileResult Index()
+        {
+            return File("dizin", "application/msword");
+
+         //   return File("dizin", "application/msword", "dosyaadi.doc");
+         
+         //   return File("dizin", "text/plain");
+
+           // return File("dizin", "text/plain", "goliq.txt");
+
+            //return new FilePathResult("dizin", "text/plain")
+           // {
+             //   FileDownloadName = "dosyaadi"
+            //};
+        }
+
+        public FileContentResult Index2()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(Server.MapPath("dizin"));
+
+            return File(bytes, "text/plain", "dosyaadi");
+        }
+
+        public ActionResult Index3()
+        {
+            var fileStream = new FileStream(Server.MapPath("dizin"), FileMode.Open);
+            return File(fileStream, "dosyaadi");
+        }
+    }
+}
