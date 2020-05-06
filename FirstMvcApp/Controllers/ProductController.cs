63f2532acfd1370cdda3c500526c37773a20cf0f
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-06 19:54:54 +0300

    custom action method selector ve jquery ajax işlemleri yapıldı.

---
+++ FirstMvcApp/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class Category
+    {
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+    public class AjaxMethodAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request.IsAjaxRequest();
+        } 
+    }
+    public class ProductController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        // GET: Json
+        [AjaxMethod]
+        public JsonResult GetAll()
+        {
+            var products = new List<Category>
+            {
+                new Category
+                {
+                     ProductName="Laptop",
+                      UnitPrice=700,
+                },
+                new Category
+                {
+                    ProductName="Mouse",
+                     UnitPrice=5
+                }
+            };
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
