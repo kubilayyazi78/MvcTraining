5f11e1b7328fddf7c877f4bba23cc3529582b5c3
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-08 23:55:39 +0300

    Model binding baslandı.

---
+++ ModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+
+            return Content(string.Format("Controller : {0} -  Action {1}",controller,action));
+        }
+    }
+}
