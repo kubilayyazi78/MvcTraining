f1be9e082a197a149a1eee541c3da808f36dbc17
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-08 21:16:11 +0300

    tempdata viewdata viewbag işlendi.

---
+++ FirstMvcApp/Controllers/NewProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class NewProductController : Controller
+    {
+        // GET: NewProduct
+        public ActionResult Index()
+        {
+
+            ViewData["message"] = "Message from viewData";
+
+            ViewBag.MessageV = "message from viebag";
+
+            ViewBag.Numbers = new [] {1,2,3,4 };
+
+            TempData["message"] = "message from tempdata";
+
+            return RedirectToAction("Index2");
+                //return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstMvcApp/Models/ProductModels/ProductIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcApp.Models.ProductModels
+{
+    public class ProductIndexViewModel
+    {
+    }
+}
