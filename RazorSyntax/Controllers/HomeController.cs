c3eb661b80b8c3efb9f9b7c9bce7e82f0b3ca0c6
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-09 17:02:47 +0300

    Shared klasör yapısı incelendi.

--- RazorSyntax/Controllers/HomeController.cs
+++ RazorSyntax/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace RazorSyntax.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View("Index5");
         }
     }
 }
