e3cb845d366374934ba26a835d62114ebd4a2ca5
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-10 22:19:21 +0300

    Custom Helpers olusturma (for button)

---
+++ RazorSyntax/HtmlHelpers/ButtonHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorSyntax.HtmlHelpers
+{
+    public static class ButtonHelpers 
+    {
+     /*   public static MvcHtmlString Submit (this HtmlHelper helper,string text)
+        {
+            var tagBuilder = new TagBuilder("button");
+            tagBuilder.InnerHtml = text;
+
+            tagBuilder.MergeAttribute("type", "submit");
+            tagBuilder.MergeAttribute("class", "btn btn-primary pull-right");
+
+            return MvcHtmlString.Create(tagBuilder.ToString());
+        }
+        */
+        public static MvcHtmlString Submit(this HtmlHelper helper, string text,object htmlAttributes)
+        {
+            var tagBuilder = new TagBuilder("button");
+            tagBuilder.InnerHtml = text;
+
+            tagBuilder.MergeAttribute("type", "submit");
+            //tagBuilder.MergeAttribute("class", "btn btn-primary pull-right");
+            tagBuilder.MergeAttributes(HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes));
+
+            return MvcHtmlString.Create(tagBuilder.ToString());
+        }
+    }
+}
