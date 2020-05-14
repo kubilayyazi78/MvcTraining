9a83a2e75354b957e6612ef0c745491d9e87bf95
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-15 00:27:34 +0300

    Filtering başlandı, childactiononly işlendi

---
+++ Filtering/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filtering.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult Login()
+        {
+            var model = "Belhanda";
+
+            return Content(string.Format("<b>Welcome {0}", model));
+        }
+    }
+}
