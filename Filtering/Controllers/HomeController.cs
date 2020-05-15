f3dd8455b6b53afccc07fb6ac436977466dd4404
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-16 02:06:54 +0300

    CustomFilters tamamlandı.

---
+++ Filtering/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Filtering.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filtering.Controllers
+{
+    public class HomeController : Controller
+    {
+        [CustomFilter]
+        public ActionResult Index()
+        {
+
+            Debug.WriteLine("Belhanda");
+            return View();
+        }
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            base.OnException(filterContext);
+        }
+    }
+    
+}
