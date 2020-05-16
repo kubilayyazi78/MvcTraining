4e67f71bb59dc932a6fc768e4ee43512b8b07121
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-16 22:42:20 +0300

    Security tamamlandı.

---
+++ Security/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Security.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Security.Controllers
+{
+    public class HomeController : Controller
+    {
+        [ValidateAntiForgeryToken]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ValidateInput(false)]
+        public ActionResult Index2(string user)
+        {
+            //user.about mesela ilgili alan için yapılır.
+            user = Sanitizer.GetSafeHtmlFragment(user);
+
+            return View();
+        }
+    }
+}
