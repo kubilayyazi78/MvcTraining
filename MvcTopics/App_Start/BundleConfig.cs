eeec53d89e8eafe6bf4d2060cf213e53a355cd73
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-17 00:55:42 +0300

    Bundle Tamamlandı.

---
+++ MvcTopics/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MvcTopics.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection  bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery-1.8.0.min.js"));
+            bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery-{version}.min.js"));
+            bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery*"));//* başlayanları getir
+            bundles.Add(new StyleBundle("~/bundle/style").Include("~/content/css/site.css"));
+
+          //  BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- MvcTopics/Global.asax.cs
+++ MvcTopics/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MvcTopics.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MvcTopics
@@ -13,6 +15,8 @@ namespace MvcTopics
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
