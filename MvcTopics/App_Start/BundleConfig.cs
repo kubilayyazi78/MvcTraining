0431eb4dbcf1d054a56d005d1e493da5118d44de
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-17 01:27:17 +0300

    Minification tamamlandı.

--- MvcTopics/App_Start/BundleConfig.cs
+++ MvcTopics/App_Start/BundleConfig.cs
@@ -13,9 +13,12 @@ namespace MvcTopics.App_Start
             bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery-1.8.0.min.js"));
             bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery-{version}.min.js"));
             bundles.Add(new ScriptBundle("~/bundle/scripts").Include("~/Scripts/jquery*"));//* başlayanları getir
-            bundles.Add(new StyleBundle("~/bundle/style").Include("~/content/css/site.css"));
 
-          //  BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundle/scripts").IncludeDirectory("~/Scripts/","JavaScript*",true));//alt klasörler dahil edildi.
+
+            bundles.Add(new StyleBundle("~/bundle/style").Include("~/content/css/site.css").Include("~/ content/css/site.css",new CssRewriteUrlTransform()));
+
+            BundleTable.EnableOptimizations = true;//minification devreye girer
         }
     }
-}
+}  
