caa5452a145de8530d964b9d8e7934d6f8d20747
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-17 00:09:51 +0300

    Areas tamamlandı.

--- MvcTopics/App_Start/RouteConfig.cs
+++ MvcTopics/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace MvcTopics
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "MvcTopics.Controllers" }
             );
         }
     }
