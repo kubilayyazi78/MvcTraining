e08872ba4eac96b5c2a9039d2fa4203c7dd33ae3
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-05-17 16:13:46 +0300

    multiple delete tamamlandı.

---
+++ Multiple/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Multiple.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Delete(List<int> ids)
+        {
+            //using ile veri tabanı bağlantısı new leyerek nort
+
+            //using (var context = new NorthwindContext()) { 
+
+            foreach (var id in ids)
+            {
+                //var product =context.products.find(id)
+                //context.products.remove(product)
+                //context.savechanges();
+            }
+            //}
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
